8d6511e5e3cd95e55edee13671847028c9430041
Gabriel Bénédict <gbndict@gmail.com>
2020-11-11 14:17:06 +0100

    i -> images

--- VLAP/keepAvailableImagesInTab.py
+++ VLAP/keepAvailableImagesInTab.py
@@ -2,16 +2,16 @@ import pandas as pd
 import os
 
 def keepAvailableImagesInTable(imageDir, labelDir):
-    i = pd.DataFrame(os.listdir(imageDir), columns = ["imdbId"])
-    i.replace(".jpg", "", regex = True, inplace = True)
-    i = i[i['imdbId'].astype(str).str.isdigit()]
+    images = pd.DataFrame(os.listdir(imageDir), columns = ["imdbId"])
+    images.replace(".jpg", "", regex = True, inplace = True)
+    images = images[images['imdbId'].astype(str).str.isdigit()]
 
     movies = pd.read_csv(labelDir, encoding="ISO-8859-1")
 
     movies['imdbId'] = movies['imdbId'].astype('int')
-    i['imdbId'] = i['imdbId'].astype('int')
+    images['imdbId'] = images['imdbId'].astype('int')
 
-    movies = pd.merge(movies, d, on = "imdbId", how = "inner")
+    movies = pd.merge(movies, images, on = "imdbId", how = "inner")
     movies['imdbId'] = movies['imdbId'].astype('object')
 
     return movies
