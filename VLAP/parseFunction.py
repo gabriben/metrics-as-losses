eb4f70c54866541a0138416ee439ff8c973759fa
Gabriel Bénédict <gbndict@gmail.com>
2020-11-27 08:40:49 +0100

    -globals

--- VLAP/parseFunction.py
+++ VLAP/parseFunction.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import globals
 
 def parseFunction(filename, label):
     """Function that returns a tuple of normalized image array and labels array.
@@ -10,9 +9,9 @@ def parseFunction(filename, label):
     # Read an image from a file
     image_string = tf.io.read_file(filename)
     # Decode it into a dense vector
-    image_decoded = tf.image.decode_jpeg(image_string, channels=globals.CHANNELS)
+    image_decoded = tf.image.decode_jpeg(image_string, channels=CHANNELS)
     # Resize it to fixed shape
-    image_resized = tf.image.resize(image_decoded, [globals.IMG_SIZE, globals.IMG_SIZE])
+    image_resized = tf.image.resize(image_decoded, [IMG_SIZE, IMG_SIZE])
     # Normalize it from [0, 255] to [0.0, 1.0]
     image_normalized = image_resized / 255.0
 
