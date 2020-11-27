d3f749b489832e3e6fadc060dd95059b713e8de7
Gabriel Bénédict <gbndict@gmail.com>
2020-11-27 08:49:23 +0100

    globals in hypperparams

--- VLAP/hyperparameters.py
+++ VLAP/hyperparameters.py
@@ -1,3 +1,8 @@
+global BATCH_SIZE
+global SHUFFLE_BUFFER_SIZE
+global IMG_SIZE
+global CHANNELS
+
 BATCH_SIZE = 256 # Big enough to measure an F1-score
 SHUFFLE_BUFFER_SIZE = 1024 # Shuffle the training data by a chunck of 1024 observations
 IMG_SIZE = 224 # Specify height and width of image to match the input format of the model
