988e05214146e72e11dc99f395a1c449cf11fabe
Gabriel Bénédict <gbndict@gmail.com>
2020-11-30 09:46:37 +0100

    inject n labels

--- VLAP/attachHead.py
+++ VLAP/attachHead.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from .hyperparameters import *
 
-def attachHead(pretrainedNet, lastActivation):
+def attachHead(pretrainedNet, N_LABELS, lastActivation):
     model = tf.keras.Sequential([
         pretrainedNet,
         layers.Dense(1024, activation='relu', name='hidden_layer'),
