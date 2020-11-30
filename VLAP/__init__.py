0f1d8f3d32dbc792f62e2696ed683500b05ffeb1
Gabriel Bénédict <gbndict@gmail.com>
2020-11-30 09:36:41 +0100

    import in init pretrained net

--- VLAP/__init__.py
+++ VLAP/__init__.py
@@ -4,3 +4,5 @@ from .binarize import binarize
 from .parseFunction import parseFunction
 from .createDataset import createDataset
 from .hyperparameters import *
+from .loadNet import loadNet
+from attachHead import attachHead
