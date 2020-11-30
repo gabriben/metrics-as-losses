fa1c1818291e20e0b6c3249fb6b1b5039c3e48b6
Gabriel Bénédict <gbndict@gmail.com>
2020-11-30 16:55:09 +0100

    module

--- VLAP/__init__.py
+++ VLAP/__init__.py
@@ -7,18 +7,23 @@
 # __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 
 
-from os.path import splitext,join,basename,dirname
-from glob import glob  
-#get all *.py filenames in __file__'s folder that are not __file__.
-files=[splitext(f)[0] for f in glob(join(dirname(__file__), '*.py')) 
-       if f != basename(__file__)]
+# from os.path import splitext,join,basename,dirname
+# from glob import glob  
+# #get all *.py filenames in __file__'s folder that are not __file__.
+# files=[splitext(f)[0] for f in glob(join(dirname(__file__), '*.py')) 
+#        if f != basename(__file__)]
+# from importlib import import_module
+# for f in files:
+#     import_module("."+  basename(f),"VLAP") #import archive.f 
+#     globals()[f]=getattr(globals[f],f) #assign the function f.f to variable f 
+
+
+from os.path import dirname, basename, isfile, join
+import glob
+modules = glob.glob(join(dirname(__file__), "*.py"))
 from importlib import import_module
-for f in files:
-    import_module("."+  basename(f),"VLAP") #import archive.f 
-    globals()[f]=getattr(globals[f],f) #assign the function f.f to variable f 
+for f in modules:
+    import_module("."+  f, "VLAP") #import archive.f 
+    globals()[f]=getattr(globals[f],f) #assign the function f.f to
 
-
-# from os.path import dirname, basename, isfile, join
-# import glob
-# modules = glob.glob(join(dirname(__file__), "*.py"))
-# __all__ = [ basename(f)[:-3] + '.' + basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+    # __all__ = [ basename(f)[:-3] + '.' + basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
