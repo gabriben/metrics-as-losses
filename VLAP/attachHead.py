9bfbb5e00a43b6d86ea69ea3aa05631f8056ffd1
Gabriel Bénédict <gbndict@gmail.com>
2020-11-30 09:41:47 +0100

    last activation

--- VLAP/attachHead.py
+++ VLAP/attachHead.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
 
-def attachHead():
+def attachHead(pretrainedNet, lastActivation):
     model = tf.keras.Sequential([
         pretrainedNet,
         layers.Dense(1024, activation='relu', name='hidden_layer'),
-        layers.Dense(N_LABELS, activation='sigmoid', name='output')
+        layers.Dense(N_LABELS, activation=lastActivation, name='output')
     ])
 
     model.summary()
