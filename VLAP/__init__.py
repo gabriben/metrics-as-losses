44fe9471a0e4043cf50d0d839fdcfc45c39a9009
Gabriel Bénédict <gbndict@gmail.com>
2020-11-30 15:16:38 +0100

    try import all in init

--- VLAP/__init__.py
+++ VLAP/__init__.py
@@ -1,8 +1,13 @@
-from .keepAvailableImagesInTab import keepAvailableImagesInTable
-from .removeRareLabels import removeRareLabels
-from .binarize import binarize
-from .parseFunction import parseFunction
-from .createDataset import createDataset
-from .hyperparameters import *
-from .loadNet import loadNet
-from .attachHead import attachHead
+# from .keepAvailableImagesInTab import keepAvailableImagesInTable
+# from .removeRareLabels import removeRareLabels
+# from .binarize import binarize
+# from .parseFunction import parseFunction
+# from .createDataset import createDataset
+# from .hyperparameters import *
+# from .loadNet import loadNet
+# from .attachHead import attachHead
+
+from os.path import dirname, basename, isfile, join
+import glob
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
