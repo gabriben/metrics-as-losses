0b5cc948e93d39e2d6fe9f7fee2d272b4765b4c5
Gabriel Bénédict <gbndict@gmail.com>
2020-11-30 19:22:10 +0100

    traininging

--- VLAP/train.py
+++ VLAP/train.py
@@ -6,14 +6,17 @@ from time import time
 from .attachHead import attachHead
 from .printTime import printTime
 from .hyperparameters import *
+from .macroSoftF1 import macroSoftF1
+from .sigmoidF1 import sigmoidF1
+from .macroF1 import macroF1
 
 def train(pretrainedNet, XYTrain, X_val, y_val_bin, nLabels):
     model = attachHead(pretrainedNet, nLabels)
 
     model.compile(
       optimizer=tf.keras.optimizers.Adam(learning_rate=LR),
-      loss= getattr(VLAP, LOSS_FUNCTION),
-      metrics=[getattr(VLAP, METRIC)])
+      loss= getattr(LOSS_FUNCTION, LOSS_FUNCTION),
+      metrics=[getattr(METRIC, METRIC)])
 
     mlflow.tensorflow.autolog()
 
