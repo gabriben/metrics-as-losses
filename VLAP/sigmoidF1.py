89a14aab57d2d4395dc6c0eb0bab76a3e3b3d438
Gabriel Bénédict <gbndict@gmail.com>
2020-11-30 19:35:50 +0100

    S ->sig

--- VLAP/sigmoidF1.py
+++ VLAP/sigmoidF1.py
@@ -17,10 +17,10 @@ def sigmoidF1(y, y_hat):
     y_hat = tf.cast(y_hat, tf.float32)
     b = tf.constant(S, tf.float32)
     c = tf.constant(E, tf.float32)
-    S = 1 / (1 + tf.math.exp(b * (y_hat + c)))
-    tp = tf.reduce_sum(S * y, axis=0)
-    fp = tf.reduce_sum(S * (1 - y), axis=0)
-    fn = tf.reduce_sum((1 - S) * y, axis=0)
+    sig = 1 / (1 + tf.math.exp(b * (y_hat + c)))
+    tp = tf.reduce_sum(sig * y, axis=0)
+    fp = tf.reduce_sum(sig * (1 - y), axis=0)
+    fn = tf.reduce_sum((1 - sig) * y, axis=0)
 
     sigmoid_f1 = 2*tp / (2*tp + fn + fp + 1e-16)
     cost = 1 - sigmoid_f1 # reduce 1 - soft-f1 in order to increase soft-f1
