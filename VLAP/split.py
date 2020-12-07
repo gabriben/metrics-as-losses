36861a3e2952e15bfe3ff8b66ebd2315ed41eb8f
Gabriel Bénédict <gbndict@gmail.com>
2020-12-07 14:19:59 +0100

    return appended lists of splits

--- VLAP/split.py
+++ VLAP/split.py
@@ -27,7 +27,7 @@ def split(X, y, s, r):
         y_splitted.append(y[(last + 1):p])
         last = p
 
-    return([X_splitted, y_splitted])
+    return(X_splitted.append(y_splitted))
 
 # example
 # X = pd.DataFrame({'col1' : np.arange(103), 'col2':np.arange(103)})
