b790a6427ee7503020b54b3eab1414cc98af598b
Gabriel Bénédict <gbndict@gmail.com>
2020-12-07 13:58:26 +0100

    split reborn

--- archive/split_UNUSED.py
+++ archive/split_UNUSED.py
@@ -1,33 +1,48 @@
+import numpy as np
+import pandas as pd
+from numpy.random import MT19937
+from numpy.random import RandomState, SeedSequence
 
-# def split(X, y, s, r):
-#     i = np.arange(len(X.index))
-#     np.random.shuffle(i)
+def split(X, y, s, r):
 
-
-
-
-
+    rs = RandomState(MT19937(SeedSequence(r)))
     
-#   X_train, X_val, y_train, y_val = train_test_split(movies['imdbId'], movies['Genre'], test_size=0.2, random_state=r)
+    i = X.index.to_numpy()
+    np.random.shuffle(i, )
+    P = np.rint(np.multiply(i.size, s))
+    P[-1] = len(i) - np.sum(P[:-1])
+    P = np.cumsum(P)
+    P = P - 1
+    P = [int(p) for p in P]
 
+    last = -1
+    X_splitted = []
+    for j, p in enumerate(P): 
+        X_splitted.append(X[(last + 1):p])
+        last = p
 
-#   print("Number of posters for training: ", len(X_train))
-#   print("Number of posters for validation: ", len(X_val))
+    last = -1
+    y_splitted = []
+    for j, p in enumerate(P): 
+        y_splitted.append(y[(last + 1):p])
+        last = p
 
+    return([X_splitted, y_splitted])
 
-# import numpy as np
-# X = np.arange(103)
-# i = np.arange(103)
+# example
+# X = pd.DataFrame({'col1' : np.arange(103), 'col2':np.arange(103)})
+# # X = np.arange(103)
+# i = X.index.to_numpy()
 # s = [0.2, 0.2, 0.6]
 # np.random.shuffle(i)
 # P = np.rint(np.multiply(i.size, s))
-# P[-1] = 103 - np.sum(p[:-1])
+# P[-1] = len(i) - np.sum(P[:-1])
 # P = np.cumsum(P)
 # P = P - 1
+# P = [int(p) for p in P]
 
 # last = -1
+# X_splitted = []
 # for j, p in enumerate(P): 
-#     X[last]
-
-# if np.sum(s) == 1.0:
-    
+#     X_splitted.append(X[(last + 1):p])
+#     last = p
