be64be2e3d9b410d1e824fe519b45c5025df9282
Gabriel Bénédict <gbndict@gmail.com>
2020-12-17 09:56:49 +0100

    focalLoss make it work

--- VLAP/focalLoss.py
+++ VLAP/focalLoss.py
@@ -1,4 +1,4 @@
 import tensorflow_addons as tfa
 
-def focalLoss():
-    return tfa.losses.SigmoidFocalCrossEntropy
+def focalLoss(y, y_hat):
+    return tfa.losses.SigmoidFocalCrossEntropy(y, y_hat)
