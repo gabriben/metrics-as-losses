d6fa12755f2290c61bd5084ad4eaef079b8dce96
Gabriel Bénédict <gbndict@gmail.com>
2021-01-20 10:35:41 +0100

    no dropout in config

--- VLAP/loadNet.py
+++ VLAP/loadNet.py
@@ -15,6 +15,7 @@ def loadNet(modelURL, numClasses, unfreezePretrain = False, fromHuggingFace = Fa
 
         print(f'Number of Classes: {numClasses}')
         config.num_labels = numClasses
+        config.seq_classif_dropout = 0
         print(config)
         pretrainedNet = TFDistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased', config = config) # TFBertForSequenceClassification
         pretrainedNet.layers[0].trainable = unfreezePretrain
