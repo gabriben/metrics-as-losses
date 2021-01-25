3e826637ce58d7f472272610e2b48120f706c779
Gabriel Bénédict <gbndict@gmail.com>
2021-01-25 15:32:52 +0100

    stage hammingScor duh

---
+++ VLAP/hammingScore.py
@@ -0,0 +1,26 @@
+# Code by https://stackoverflow.com/users/1953100/william
+# Source: https://stackoverflow.com/a/32239764/395857
+# License: cc by-sa 3.0 with attribution required
+
+import numpy as np
+
+def hammingScore(y_true, y_pred, normalize=True, sample_weight=None):
+    '''
+    Compute the Hamming score (a.k.a. label-based accuracy) for the multi-label case
+    https://stackoverflow.com/q/32239577/395857
+    '''
+    acc_list = []
+    for i in range(y_true.shape[0]):
+        set_true = set( np.where(y_true[i])[0] )
+        set_pred = set( np.where(y_pred[i])[0] )
+        #print('\nset_true: {0}'.format(set_true))
+        #print('set_pred: {0}'.format(set_pred))
+        tmp_a = None
+        if len(set_true) == 0 and len(set_pred) == 0:
+            tmp_a = 1
+        else:
+            tmp_a = len(set_true.intersection(set_pred))/\
+                    float( len(set_true.union(set_pred)) )
+        #print('tmp_a: {0}'.format(tmp_a))
+        acc_list.append(tmp_a)
+    return np.mean(acc_list)
