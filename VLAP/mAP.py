2173242807d1b74fe145e224f81bef8ba7c832f5
Gabriel Bénédict <gbndict@gmail.com>
2021-08-25 10:40:07 +0200

    mAP from ASL paper

---
+++ VLAP/mAP.py
@@ -0,0 +1,40 @@
+#https://github.com/Alibaba-MIIL/ASL/blob/7114637713619f01906ed73bbfa182565cd3a77d/src/helper_functions/helper_functions.py#L49
+
+import numpy as np
+
+def average_precision(output, target):
+    epsilon = 1e-8
+
+    # sort examples
+    indices = output.argsort()[::-1]
+    # Computes prec@i
+    total_count_ = np.cumsum(np.ones((len(output), 1)))
+
+    target_ = target[indices]
+    ind = target_ == 1
+    pos_count_ = np.cumsum(ind)
+    total = pos_count_[-1]
+    pos_count_[np.logical_not(ind)] = 0
+    pp = pos_count_ / total_count_
+    precision_at_i_ = np.sum(pp)
+    precision_at_i = precision_at_i_ / (total + epsilon)
+
+    return precision_at_i
+
+def mAP(targs, preds):
+    """Returns the model's average precision for each class
+    Return:
+        ap (FloatTensor): 1xK tensor, with avg precision for each class k
+    """
+
+    if np.size(preds) == 0:
+        return 0
+    ap = np.zeros((preds.shape[1]))
+    # compute average precision for each class
+    for k in range(preds.shape[1]):
+        # sort scores
+        scores = preds[:, k]
+        targets = targs[:, k]
+        # compute average precision
+        ap[k] = average_precision(scores, targets)
+    return 100 * ap.mean()
