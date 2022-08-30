d94e2d7d9c681fb5eb9d869d354616eec543ea75
Gabriel Bénédict <gbndict@gmail.com>
2022-08-30 22:36:22 +0200

    define y as second variable

--- VLAP/thresholding.py
+++ VLAP/thresholding.py
@@ -1,14 +1,14 @@
 from sklearn.metrics import f1_score
 import numpy as np
 
-def avgThresh(preds):
+def avgThresh(preds, y):
 
     predsThresh = preds[..., None] < preds[:, None, :]
 
     F1 = np.zeros(preds.shape)
 
     for i in range(predsThresh.shape[1]):
-      F1[:, i] = f1_score(predsThresh[:, i, :].T, y_train_bin.T, average = None)
+      F1[:, i] = f1_score(predsThresh[:, i, :].T, y.T, average = None)
 
     maxThresholds = np.argmax(F1, axis = 1)
     avg_thresh = np.mean(preds[:, maxThresholds[0]])
