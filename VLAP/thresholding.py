7f50ecd70e004efd97d31dbda56c2ace9c6e4d12
Gabriel Bénédict <gbndict@gmail.com>
2022-10-06 15:19:27 +0200

    avgthresh columnwise and return all thresholds

--- VLAP/thresholding.py
+++ VLAP/thresholding.py
@@ -1,23 +1,27 @@
 from sklearn.metrics import f1_score
 import numpy as np
 
-def avgThresh(preds, y, columnwise = False):
+def avgThresh(preds, y, columnwise = False, return_all_thresholds = False):
 
     if columnwise:
-        y = y.T
-        preds = preds.T
+        preds, y = preds.T, y.T
     
     predsThresh = preds[..., None] < preds[:, None, :]
 
     F1 = np.zeros(preds.shape)
-
-    for i in range(predsThresh.shape[1]):
+  
+    if columnwise:
+      F1dim = predsThresh.shape[0]
+    else:
+      F1dim = predsThresh.shape[1]
+    
+    for i in range(F1dim):
       F1[:, i] = f1_score(predsThresh[:, i, :].T, y.T, average = None)
 
     maxThresholds = np.argmax(F1, axis = 1)
-    avg_thresh = np.mean(preds[:, maxThresholds[0]])
-
-    if columnwise:
-        return avg_thresh.T
+    
+    if return_all_thresholds:
+      return preds[:, maxThresholds[0]]
     else:
-        return avg_thresh
+      avg_thresh = np.mean(preds[:, maxThresholds[0]])
+      return avg_thresh
